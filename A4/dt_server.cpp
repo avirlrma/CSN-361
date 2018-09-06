6ed0fcf597035fde9714fb3c6c7cfa5466eebfb1
avirlrma <aviral1701@gmail.com>
2018-09-06 22:12:56 +0530

    daytime server and client

---
+++ A4/dt_server.cpp
@@ -0,0 +1,72 @@
+// Server side C/C++ program to demonstrate Socket programming 
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#include <ctype.h>
+#include <time.h>
+
+#define PORT 8080 
+int main(int argc, char const *argv[]) 
+{ 
+    int server_fd, new_socket, valread; 
+    struct sockaddr_in address; 
+    int opt = 1; 
+    int addrlen = sizeof(address); 
+       
+    // Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+                                                  &opt, sizeof(opt))) 
+    { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    } 
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons( PORT ); 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (bind(server_fd, (struct sockaddr *)&address,  
+                                 sizeof(address))<0) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    }
+    while(1){
+    if (listen(server_fd, 3) < 0) 
+    { 
+        perror("listen"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
+                       (socklen_t*)&addrlen))<0) 
+    { 
+        perror("accept"); 
+        exit(EXIT_FAILURE); 
+    }
+    else{
+    int pid = fork();
+    if(pid==0){
+        char *hello;
+        char buffer[1024] = {0};
+        time_t     now = time(0);
+        struct tm  tstruct;
+        char       buf[80];
+        tstruct = *localtime(&now);
+        strftime(buffer, sizeof(buffer), "%Y-%m-%d.%X", &tstruct);
+        send(new_socket , buffer , strlen(buffer) , 0 ); 
+        return 0;
+        }
+    }
+}
+    return 0; 
+} 
